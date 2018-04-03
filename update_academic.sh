f0f9f84da74927051758e806c448d1eaf4e03442
Shaunak De <shaunak@gmail.com>
2018-04-03 17:28:19 +0530

    Initial

--- update_academic.sh
+++ update_academic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Display available updates to Academic.
-cd themes/academic
+cd themes/academic-sd
 git fetch
 git log --pretty=oneline --abbrev-commit --decorate HEAD..origin/master
 cd ../../
